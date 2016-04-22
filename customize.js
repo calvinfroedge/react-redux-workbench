7df5b95435f8d72b18a9f3c9f34efaba0e4683b0
Calvin Froedge <calvinfroedge@gmail.com>
2016-04-22 13:13:59 -0400

    customize should set version to 0.0.1

--- customize.js
+++ customize.js
@@ -7,16 +7,28 @@ if(!PROJECT_NAME || !PROJECT_DESCRIPTION || !GITHUB_USERNAME || !AUTHOR_NAME || 
   process.exit();
 }
 
+RegExp.quote = function(str) {
+  return str.replace(/([.?*+^$[\]\\(){}-])/g, "\\$1");
+};
+
+String.prototype.replaceAll = function(search, replace)
+{
+  var re = new RegExp(RegExp.quote(search),"g");
+  return this.replace(re, replace);
+};
+
 let oldName = 'react-redux-workbench';
 let packageJSON = fs.readFileSync('./.package.json').toString();
+let oldVersion = '"version": "'+JSON.parse(packageJSON).version+'"';
+let newVersion = '"version": "0.0.1"';
 let gitConfig = fs.readFileSync('./.gitconfig').toString();
 let webpackConfig = fs.readFileSync('./.webpack.config.js').toString();
 let projectName = PROJECT_NAME;
 let repo = [GITHUB_USERNAME, `${projectName}`].join('/');
-let gitConfigNew = gitConfig.replace(`calvinfroedge/${oldName}`, repo);
-let file = projectName.replace(' ', '-').toLowerCase();
-let packageJSONNEW = packageJSON.replace(oldName, file).replace('Calvin Froedge', AUTHOR_NAME).replace('calvinfroedge@gmail.com', AUTHOR_EMAIL).replace('description-customize-me', PROJECT_DESCRIPTION);
-let webpackConfigNEW = webpackConfig.replace('ReactReduxWorkbench', projectName.replace(' ', '')).replace(oldName, file);
+let gitConfigNew = gitConfig.replaceAll(`calvinfroedge/${oldName}`, repo);
+let file = projectName.replaceAll(' ', '-').toLowerCase();
+let packageJSONNEW = packageJSON.replaceAll(oldName, file).replaceAll('Calvin Froedge', AUTHOR_NAME).replaceAll('calvinfroedge@gmail.com', AUTHOR_EMAIL).replaceAll('description-customize-me', PROJECT_DESCRIPTION).replaceAll(oldVersion, newVersion);
+let webpackConfigNEW = webpackConfig.replaceAll('ReactReduxWorkbench', projectName.replaceAll(' ', '')).replaceAll(oldName, file);
 fs.writeFileSync('./.git/config', gitConfigNew);
 fs.writeFileSync('./package.json', packageJSONNEW);
 console.info('Wrote updates to package.json, .git/config, and wepback.config.js.\n');
